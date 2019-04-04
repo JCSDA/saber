3243d545340a4061c83e0e7c97ad614c08faef17
Yannick Trémolet <tremolet@ucar.edu>
2019-04-04 17:50:02 -0600

    Initial commit

---
+++ cmake/saber_compiler_flags.cmake
@@ -0,0 +1,48 @@
+# (C) Copyright 2019 UCAR
+# 
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+
+if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
+  add_definitions( -DNDEBUG )
+endif( )
+
+#######################################################################################
+# Fortran
+#######################################################################################
+
+if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
+  include( compiler_flags_GNU_Fortran )
+elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
+  include( compiler_flags_Intel_Fortran )
+elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
+  include( compiler_flags_XL_Fortran )
+elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
+  include( compiler_flags_Cray_Fortran )
+else()
+  message( STATUS "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
+endif()
+
+#######################################################################################
+# C
+#######################################################################################
+
+# todo
+
+#######################################################################################
+# C++
+#######################################################################################
+
+if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
+  include( compiler_flags_GNU_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
+  include( compiler_flags_Intel_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "XL" )
+  include( compiler_flags_XL_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Cray" )
+  include( compiler_flags_Cray_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
+  include( compiler_flags_Clang_CXX )
+else()
+  message( STATUS "C++ compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
+endif()
