0952f2874f79aff8c24823bea917ec88e9b1ba29
Benjamin Menetrier <benjamin.menetrier@irit.fr>
2019-11-27 10:55:44 +0100

    Add git branch

--- tools/saber_tar.sh
+++ tools/saber_tar.sh
@@ -25,6 +25,9 @@ tar -cvzf ${name}.tar.gz ${files}
 # Compute MD5 checksum
 md5sum ${name}.tar.gz > ${name}.tar.gz.md5
 
+# Get git branch
+branch=`git rev-parse --abbrev-ref HEAD`
+
 # Send to S3
-aws s3 cp ${name}.tar.gz s3://jedi-test-files/saber/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-aws s3 cp ${name}.tar.gz.md5 s3://jedi-test-files/saber/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp ${name}.tar.gz s3://jedi-test-files/saber/${branch}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp ${name}.tar.gz.md5 s3://jedi-test-files/saber/${branch}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
