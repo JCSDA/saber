9e5a91e069c2cdda944333d3bfa95a81a77ccdd1
benjaminmenetrier <benjamin.menetrier@irit.fr>
2020-03-31 14:51:45 +0200

    Bugfix for smask

--- src/saber/bump/type_geom.F90
+++ src/saber/bump/type_geom.F90
@@ -137,6 +137,12 @@ implicit none
 class(geom_type),intent(inout) :: geom ! Geometry
 
 ! Release memory
+if (allocated(geom%lon_mga)) deallocate(geom%lon_mga)
+if (allocated(geom%lat_mga)) deallocate(geom%lat_mga)
+if (allocated(geom%area_mga)) deallocate(geom%area_mga)
+if (allocated(geom%vunit_mga)) deallocate(geom%vunit_mga)
+if (allocated(geom%gmask_mga)) deallocate(geom%gmask_mga)
+if (allocated(geom%smask_mga)) deallocate(geom%smask_mga)
 if (allocated(geom%lon)) deallocate(geom%lon)
 if (allocated(geom%lon_c0a)) deallocate(geom%lon_c0a)
 if (allocated(geom%lat)) deallocate(geom%lat)
@@ -227,7 +233,10 @@ else
 end if
 
 ! No mask
-if (nam%nomask) geom%gmask_mga = .true.
+if (nam%nomask) then
+   geom%gmask_mga = .true.
+   geom%smask_mga = .true.
+end if
 
 ! Allocation
 allocate(proc_to_nmga(mpl%nproc))
@@ -625,6 +634,7 @@ allocate(geom%lat_c0a(geom%nc0a))
 allocate(geom%vunit_c0a(geom%nc0a,geom%nl0))
 allocate(geom%mask_c0a(geom%nc0a,geom%nl0))
 allocate(geom%mask_hor_c0a(geom%nc0a))
+allocate(geom%smask_c0a(geom%nc0a,geom%nl0))
 
 ! Define other fields
 geom%lon_c0a = geom%lon(geom%c0a_to_c0)
@@ -632,6 +642,9 @@ geom%lat_c0a = geom%lat(geom%c0a_to_c0)
 geom%vunit_c0a = geom%vunit_c0(geom%c0a_to_c0,:)
 geom%mask_c0a = geom%mask_c0(geom%c0a_to_c0,:)
 geom%mask_hor_c0a = geom%mask_hor_c0(geom%c0a_to_c0)
+
+! Communicate sampling mask
+call geom%copy_mga_to_c0a(mpl,geom%smask_mga,geom%smask_c0a)
 
 ! Allocation
 call geom%mesh%alloc(geom%nc0)
