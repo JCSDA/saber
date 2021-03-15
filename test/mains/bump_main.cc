b1fa2bb34057a935aeda88a58546b1e644055a95
Mark Miesch <miesch@ucar.edu>
2021-03-15 00:22:30 -0600

    finalize MPI in bump_main executable (#71)

--- test/mains/bump_main.cc
+++ test/mains/bump_main.cc
@@ -7,6 +7,7 @@
 
 #include <string.h>
 
+#include "eckit/mpi/Comm.h"
 #include "eckit/runtime/Main.h"
 
 extern "C" {
@@ -19,5 +20,6 @@ int main(int argc, char** argv) {
   int n1 = strlen(argv[1]);
   int n2 = strlen(argv[2]);
   bump_main_f90(n1, argv[1], n2, argv[2]);
+  eckit::mpi::finaliseAllComms();
   return 0;
 }
