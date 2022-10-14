1153a8ba4fb16c6f1cb4a03dd5d89b073bdd2273
Marek Wlasak <marekandjenny@gmail.com>
2022-10-14 09:14:57 +0100

    temporary commit

--- test/quench/Fields.cc
+++ test/quench/Fields.cc
@@ -703,6 +703,7 @@ void Fields::write(const eckit::Configuration & config) const {
 
   // NetCDF output
   if (geom_->functionSpace().type() == "StructuredColumns") {
+    /*
     // StructuredColumns
     atlas::functionspace::StructuredColumns fs(geom_->functionSpace());
 
@@ -834,6 +835,7 @@ void Fields::write(const eckit::Configuration & config) const {
       // Close file
       if ((retval = nc_close(ncid))) ERR(retval);
     }
+    */
   } else if (geom_->functionSpace().type() == "NodeColumns") {
     // NodeColumns
     atlas::idx_t nb_nodes;
