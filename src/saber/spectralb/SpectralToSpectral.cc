519fa414e813fc7018bf1b93f6b655c9ca8da9f1
MayeulDestouches <mayeul.destouches@metoffice.gov.uk>
2023-02-14 14:18:11 +0000

    Add a spectral to spectral SABER block (#443)
    * Initial try
    
    * Update FieldSet
    
    * Pass FieldSet by reference, Add outField to outFieldSet
    * Update reference file
    * Explicit comments in yaml
    
    * Coding norms
    
    * Address Marek's comments
    
    * Bugfix
    
    * Trigger tests
    
    * Add mandatoryActiveVars method
    
    * Trigger test
    
    ---------
    
    Co-authored-by: Benjamin Menetrier <benjamin.menetrier@irit.fr>
    Co-authored-by: Benjamin Menetrier <30638301+benjaminmenetrier@users.noreply.github.com>

---
+++ src/saber/spectralb/SpectralToSpectral.cc
@@ -0,0 +1,217 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include <set>
+#include <vector>
+
+#include "saber/spectralb/SpectralToSpectral.h"
+
+#include "eckit/exception/Exceptions.h"
+
+namespace saber {
+namespace spectralb {
+
+// -----------------------------------------------------------------------------
+
+static SaberOuterBlockMaker<SpectralToSpectral> makerSpectralToSpectral_("spectral to spectral");
+
+// -----------------------------------------------------------------------------
+
+SpectralToSpectral::SpectralToSpectral(const oops::GeometryData & outerGeometryData,
+                                       const std::vector<size_t> & outerVariableSizes,
+                                       const oops::Variables & outerVars,
+                                       const SpectralToSpectralParameters & params,
+                                       const atlas::FieldSet & xb,
+                                       const atlas::FieldSet & fg,
+                                       const std::vector<atlas::FieldSet> & fsetVec) :
+    innerFunctionSpace_(2 * params.inputTruncation.value() - 1),
+    outerFunctionSpace_(outerGeometryData.functionSpace()),
+    innerGeometryData_(innerFunctionSpace_,
+                       outerGeometryData.fieldSet(),
+                       outerGeometryData.levelsAreTopDown(),
+                       outerGeometryData.comm()),
+    activeVars_(params.activeVars.value().value_or(outerVars))
+{
+  oops::Log::trace() << classname() << "::SpectralToSpectral starting" << std::endl;
+  oops::Log::trace() << classname() << "::SpectralToSpectral done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::multiply(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::multiply starting" << std::endl;
+  truncate_or_extend(innerFunctionSpace_, outerFunctionSpace_, fieldSet);
+  oops::Log::trace() << classname() << "::multiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::multiplyAD(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::multiplyAD starting" << std::endl;
+  truncate_or_extend(outerFunctionSpace_, innerFunctionSpace_, fieldSet);
+  oops::Log::trace() << classname() << "::multiplyAD done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::calibrationInverseMultiply(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::calibrationInverseMultiply starting" << std::endl;
+  throw eckit::NotImplemented(Here());
+  oops::Log::trace() << classname() << "::calibrationInverseMultiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::truncate_or_extend(const atlas::functionspace::Spectral & input_fspace,
+                                            const atlas::functionspace::Spectral & output_fspace,
+                                            atlas::FieldSet & fieldSet) const {
+  // Decides whether fieldSet should be truncated or extented, based on function spaces.
+  oops::Log::trace() << classname() << "::truncate_or_extend starting" << std::endl;
+  const int input_truncation = input_fspace.truncation();
+  const int output_truncation = output_fspace.truncation();
+  if (input_truncation < output_truncation) {
+    // Extend by zero-padding
+    truncateAD(input_fspace, output_fspace, fieldSet);
+  } else if (input_truncation > output_truncation) {
+    // Truncate
+    truncate(input_fspace, output_fspace, fieldSet);
+  } else {
+    // Same truncation, pass.
+  }
+  oops::Log::trace() << classname() << "::truncate_or_extend done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::truncate(const atlas::functionspace::Spectral & input_functionspace,
+                                  const atlas::functionspace::Spectral & output_functionspace,
+                                  atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::truncate starting" << std::endl;
+
+  // Initialize empty field set
+  atlas::FieldSet outFieldSet;
+
+  const auto zonal_wavenumbers_in = input_functionspace.zonal_wavenumbers();
+  const auto zonal_wavenumbers_out = output_functionspace.zonal_wavenumbers();
+  const atlas::idx_t nb_zonal_wavenumbers_in = zonal_wavenumbers_in.size();
+  const atlas::idx_t nb_zonal_wavenumbers_out = zonal_wavenumbers_out.size();
+  const std::size_t truncation_in = input_functionspace.truncation();
+  const std::size_t truncation_out = output_functionspace.truncation();
+  ASSERT(truncation_in > truncation_out);
+
+  for (auto field : fieldSet) {
+    if (!activeVars_.has(field.name())) {
+      // Skip passive variable
+      outFieldSet.add(field);
+    } else {
+      // active variable
+      atlas::Field outField = output_functionspace.createField<double>(
+                              atlas::option::name(field.name()) |
+                              atlas::option::levels(field.levels()));
+      auto inFieldView = atlas::array::make_view<double, 2>(field);
+      auto outFieldView = atlas::array::make_view<double, 2>(outField);
+
+      // We loop over indexes of the field to be truncated
+      int jnode_in = 0, jnode_out = 0;
+      for (int jm=0; jm < nb_zonal_wavenumbers_in; ++jm) {
+        const atlas::idx_t m = zonal_wavenumbers_in(jm);
+        for (std::size_t n = m; n <= truncation_in; ++n) {
+          for (auto & part : std::set<std::string>{"real", "imaginary"}) {
+            if (jm < nb_zonal_wavenumbers_out && n <= truncation_out) {
+                // In common zone, copy values.
+                for (atlas::idx_t jlevel = 0; jlevel < field.shape(1); ++jlevel) {
+                  outFieldView(jnode_out, jlevel) = inFieldView(jnode_in, jlevel);
+                }
+                ++jnode_in; ++jnode_out;
+            } else {
+                // In truncated zone
+                ++jnode_in;
+            }
+          }
+        }
+      }
+      outFieldSet.add(outField);
+    }
+  }
+  fieldSet = outFieldSet;
+
+  oops::Log::trace() << classname() << "::truncate done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::truncateAD(const atlas::functionspace::Spectral & input_functionspace,
+                                    const atlas::functionspace::Spectral & output_functionspace,
+                                    atlas::FieldSet & fieldSet) const {
+  // The adjoint of spectral truncation is extension by zero-padding.
+  oops::Log::trace() << classname() << "::truncateAD starting" << std::endl;
+  // Initialize empty field set
+  atlas::FieldSet outFieldSet;
+
+  const auto zonal_wavenumbers_in = input_functionspace.zonal_wavenumbers();
+  const auto zonal_wavenumbers_out = output_functionspace.zonal_wavenumbers();
+  const atlas::idx_t nb_zonal_wavenumbers_in = zonal_wavenumbers_in.size();
+  const atlas::idx_t nb_zonal_wavenumbers_out = zonal_wavenumbers_out.size();
+  const std::size_t truncation_in = input_functionspace.truncation();
+  const std::size_t truncation_out = output_functionspace.truncation();
+  ASSERT(truncation_in < truncation_out);
+
+  for (auto field : fieldSet) {
+    if (!activeVars_.has(field.name())) {
+      // Skip passive variable
+      outFieldSet.add(field);
+    } else {
+      // active variable
+      atlas::Field outField = output_functionspace.createField<double>(
+                              atlas::option::name(field.name()) |
+                              atlas::option::levels(field.levels()));
+      auto inFieldView = atlas::array::make_view<double, 2>(field);
+      auto outFieldView = atlas::array::make_view<double, 2>(outField);
+
+      // We loop over indexes of the field to be extended
+      int jnode_in = 0, jnode_out = 0;
+      for (int jm=0; jm < nb_zonal_wavenumbers_out; ++jm) {
+        const atlas::idx_t m = zonal_wavenumbers_out(jm);
+        for (std::size_t n = m; n <= truncation_out; ++n) {
+          for (auto & part : std::set<std::string>{"real", "imaginary"}) {
+            if (jm < nb_zonal_wavenumbers_in && n <= truncation_in) {
+                // In common zone, copy values.
+                for (atlas::idx_t jlevel = 0; jlevel < field.shape(1); ++jlevel) {
+                  outFieldView(jnode_out, jlevel) = inFieldView(jnode_in, jlevel);
+                }
+                ++jnode_in; ++jnode_out;
+            } else {
+                // In extension zone, pad with zeros.
+                for (atlas::idx_t jlevel = 0; jlevel < field.shape(1); ++jlevel) {
+                  outFieldView(jnode_out, jlevel) = 0.0;
+                }
+                ++jnode_out;
+            }
+          }
+        }
+      }
+      outFieldSet.add(outField);
+    }
+  }
+  fieldSet = outFieldSet;
+
+  oops::Log::trace() << classname() << "::truncateAD done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralToSpectral::print(std::ostream & os) const {
+  os << classname() << " going from truncation " << innerFunctionSpace_.truncation()
+                    << " to " << outerFunctionSpace_.truncation() << "," << std::endl;
+  os << "from size " << innerFunctionSpace_.size() << " to "
+                     << outerFunctionSpace_.size() << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace spectralb
+}  // namespace saber
---
+++ src/saber/spectralb/SpectralToSpectral.h
@@ -0,0 +1,74 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "atlas/field/FieldSet.h"
+#include "atlas/functionspace/Spectral.h"
+#include "oops/base/GeometryData.h"
+#include "oops/base/Variables.h"
+#include "saber/oops/SaberBlockParametersBase.h"
+#include "saber/oops/SaberOuterBlockBase.h"
+
+namespace saber {
+namespace spectralb {
+
+class SpectralToSpectralParameters : public SaberBlockParametersBase {
+  OOPS_CONCRETE_PARAMETERS(SpectralToSpectralParameters, SaberBlockParametersBase)
+ public:
+  oops::RequiredParameter<int> inputTruncation{"input truncation", this};
+  oops::Variables mandatoryActiveVars() const {return oops::Variables();}
+};
+
+
+class SpectralToSpectral : public SaberOuterBlockBase {
+ public:
+  static const std::string classname() {return "saber::spectralb::SpectralToSpectral";}
+
+  typedef SpectralToSpectralParameters Parameters_;
+
+  SpectralToSpectral(const oops::GeometryData &,
+                     const std::vector<size_t> &,
+                     const oops::Variables &,
+                     const SpectralToSpectralParameters &,
+                     const atlas::FieldSet &,
+                     const atlas::FieldSet &,
+                     const std::vector<atlas::FieldSet> &);
+
+  virtual ~SpectralToSpectral() = default;
+
+  const oops::GeometryData & innerGeometryData() const override {return innerGeometryData_;}
+  const oops::Variables & innerVars() const override {return activeVars_;}
+
+  void multiply(atlas::FieldSet &) const override;
+  void multiplyAD(atlas::FieldSet &) const override;
+  void calibrationInverseMultiply(atlas::FieldSet &) const override;
+
+ private:
+  void print(std::ostream &) const override;
+  void truncate_or_extend(const atlas::functionspace::Spectral &,
+                          const atlas::functionspace::Spectral &,
+                          atlas::FieldSet &) const;
+  void truncate(const atlas::functionspace::Spectral &,
+                const atlas::functionspace::Spectral &,
+                atlas::FieldSet &) const;
+  void truncateAD(const atlas::functionspace::Spectral &,
+                  const atlas::functionspace::Spectral &,
+                  atlas::FieldSet &) const;
+
+  const atlas::functionspace::Spectral innerFunctionSpace_;
+  const atlas::functionspace::Spectral outerFunctionSpace_;
+  const oops::GeometryData innerGeometryData_;
+  const oops::Variables activeVars_;
+};
+
+}  // namespace spectralb
+}  // namespace saber
