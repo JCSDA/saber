d185802c331570f79b43a8c1d72c749bdce264c0
MayeulDestouches <mayeul.destouches@metoffice.gov.uk>
2023-07-24 15:30:26 +0000

    Add horizontal localization in spectral space (#619)
    * Adding files
    
    * Add tests
    
    * First debugging attempt
    
    * Working but heterogeneous
    
    * Remove debug prints
    
    * Simplify code and tune yaml
    
    * cleaning
    
    * Update reference file
    
    * Fix randomize
    
    * Coding norms
    
    * Add testing for square root consistency
    
    * Duplicate code for real/imaginary components
    
    * More explicit double division
    
    * Convert to outer block -- Untested
    
    * Fix yaml and reference file
    
    * Rename block to Spectral Analytical Filter
    
    * More flexible yaml interface
    
    * Coding norms
    
    * More explicit yaml comment
    
    ---------
    
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

---
+++ src/saber/spectralb/SpectralAnalyticalFilter.cc
@@ -0,0 +1,231 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <string>
+
+#include "saber/spectralb/SpectralAnalyticalFilter.h"
+
+#include "atlas/array/MakeView.h"
+#include "atlas/field/Field.h"
+#include "atlas/field/FieldSet.h"
+#include "atlas/util/Earth.h"
+
+#include "eckit/exception/Exceptions.h"
+#include "eckit/mpi/Operation.h"
+
+#include "oops/util/Logger.h"
+
+#include "saber/oops/Utilities.h"
+
+// -----------------------------------------------------------------------------
+
+namespace saber {
+namespace spectralb {
+
+// -----------------------------------------------------------------------------
+
+static SaberOuterBlockMaker<SpectralAnalyticalFilter> makerSpectralAnalyticalFilter_(
+        "spectral analytical filter");
+
+// -----------------------------------------------------------------------------
+
+namespace {
+
+void gaussianShape(std::vector<double> & wavenumbers,
+                   const double loc_rh) {
+  // Transform wavenumbers into Gaussian
+  oops::Log::info() << "Info     : Horizontal localization length is "
+                    << loc_rh / 1000.0 << " km." << std::endl;
+  const double standard_wavenumber = atlas::util::Earth::radius() / loc_rh;
+  const double factor = 1.0 / (2 * std::pow(standard_wavenumber, 2));
+  std::transform(wavenumbers.begin(), wavenumbers.end(),
+                 wavenumbers.begin(),
+                 [& factor](auto & n){return std::exp(- factor * std::pow(n, 2));});
+}
+
+// -----------------------------------------------------------------------------
+
+auto createSpectralFilter(const oops::GeometryData & geometryData,
+                          const SpectralAnalyticalFilterParameters & params) {
+  oops::Log::trace() << "saber::spectralb::createSpectralFilter starting"
+                     << std::endl;
+
+  // 1) Initialize filter as a vector of total wavenumbers
+  // -----------------------------------------------------
+  const atlas::functionspace::Spectral specFunctionSpace(geometryData.functionSpace());
+  const int truncation = specFunctionSpace.truncation();
+  std::vector<double> spectralFilter(truncation + 1);
+  std::iota(spectralFilter.begin(), spectralFilter.end(), 0);
+
+  // 2) Modify into required filter
+  // ------------------------------
+  const auto & function = params.function.value();
+  const std::string functionShape(function.getString("shape", "gaussian"));
+  if (functionShape.compare("gaussian") == 0) {
+    if (!function.has("horizontal daley length")) throw eckit::BadParameter(
+                    "horizontal daley length must be specified for Gaussian function shapes");
+
+    const double loc_rh = function.getDouble("horizontal daley length");
+    gaussianShape(spectralFilter, loc_rh);
+  } else {
+    throw eckit::BadParameter("function shape " + functionShape + " not implemented yet.",
+                              Here());
+  }
+
+  // 3) Normalize as a localization function if required
+  // ---------------------------------------------------
+  if (params.normalize.value()) {
+    // Compute total variance in spectral space before normalization.
+    double totalVarianceSpectral = 0;
+    const auto zonal_wavenumbers = specFunctionSpace.zonal_wavenumbers();
+    for (int jm = 0; jm < zonal_wavenumbers.size(); ++jm) {
+      const int m = zonal_wavenumbers(jm);
+      for (int n = m; n <= truncation; ++n) {
+        if (m == 0) {
+          totalVarianceSpectral += spectralFilter[n];
+        } else {
+          // both real and imaginary components
+          totalVarianceSpectral += 2 * spectralFilter[n];
+        }
+      }
+    }
+    geometryData.comm().allReduceInPlace(totalVarianceSpectral, eckit::mpi::sum());
+
+    // Normalize total variance to be 1
+    const double normalization = 1.0 / totalVarianceSpectral;
+    std::transform(spectralFilter.begin(), spectralFilter.end(),
+                   spectralFilter.begin(),
+                   [& normalization](auto & elem){return elem * normalization;});
+  }
+
+  // 4) Take square root
+  // -------------------
+  std::transform(spectralFilter.begin(), spectralFilter.end(),
+                 spectralFilter.begin(), [](auto & e){return std::sqrt(e);});
+
+  oops::Log::trace() << "saber::spectralb::createSpectralFilter done"
+                     << std::endl;
+  return spectralFilter;
+}
+
+}  // namespace
+
+// -----------------------------------------------------------------------------
+SpectralAnalyticalFilter::SpectralAnalyticalFilter(const oops::GeometryData & geometryData,
+                                               const oops::Variables & outerVars,
+                                               const eckit::Configuration & covarConf,
+                                               const Parameters_ & params,
+                                               const atlas::FieldSet & xb,
+                                               const atlas::FieldSet & fg,
+                                               const util::DateTime & validTimeOfXbFg)
+  : SaberOuterBlockBase(params), params_(params),
+    activeVars_(getActiveVars(params, outerVars)),
+    innerGeometryData_(geometryData),
+    innerVars_(outerVars),
+    specFunctionSpace_(geometryData.functionSpace()),
+    spectralFilter_(createSpectralFilter(geometryData, params))
+{
+  oops::Log::trace() << classname() << "::SpectralAnalyticalFilter starting " << std::endl;
+  oops::Log::trace() << classname() << "::SpectralAnalyticalFilter done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralAnalyticalFilter::multiply(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::multiply starting" << std::endl;
+
+  const auto zonal_wavenumbers = specFunctionSpace_.zonal_wavenumbers();
+  const int truncation = specFunctionSpace_.truncation();
+  const int max_zonal_wavenumber = zonal_wavenumbers.size();
+
+  for (const auto & var : activeVars_.variables()) {
+    auto view = atlas::array::make_view<double, 2>(fieldSet[var]);
+    const atlas::idx_t levels = fieldSet[var].levels();
+
+    // Element-wise multiplication
+    atlas::idx_t jnode = 0;
+    for (int jm = 0; jm < max_zonal_wavenumber; ++jm) {
+      int m = zonal_wavenumbers(jm);
+      for (int n = m; n <= truncation; ++n) {
+        // Real component
+        for (atlas::idx_t jlev = 0; jlev < levels; jlev++) {
+          view(jnode, jlev) *= spectralFilter_[n];
+        }
+        jnode++;
+        // Imaginary component
+        for (atlas::idx_t jlev = 0; jlev < levels; jlev++) {
+          view(jnode, jlev) *= spectralFilter_[n];
+        }
+        jnode++;
+      }
+    }
+  }
+
+  oops::Log::trace() << classname() << "::multiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralAnalyticalFilter::multiplyAD(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::multiplyAD starting" << std::endl;
+
+  // The block is self-adjoint:
+  multiply(fieldSet);
+
+  oops::Log::trace() << classname() << "::multiplyAD done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralAnalyticalFilter::leftInverseMultiply(atlas::FieldSet & fieldSet) const {
+  oops::Log::trace() << classname() << "::leftInverseMultiply starting" << std::endl;
+
+  const auto zonal_wavenumbers = specFunctionSpace_.zonal_wavenumbers();
+  const int truncation = specFunctionSpace_.truncation();
+  const int max_zonal_wavenumber = zonal_wavenumbers.size();
+
+  std::vector<double> filterInverse(spectralFilter_.size());
+  std::transform(spectralFilter_.begin(), spectralFilter_.end(),
+                 filterInverse.begin(), [&](auto & x){return x != 0.0 ? 1.0 / x : 0.0;});
+
+  for (const auto & var : activeVars_.variables()) {
+    auto view = atlas::array::make_view<double, 2>(fieldSet[var]);
+    const atlas::idx_t levels = fieldSet[var].levels();
+
+    // Element-wise multiplication
+    atlas::idx_t jnode = 0;
+    for (int jm = 0; jm < max_zonal_wavenumber; ++jm) {
+      int m = zonal_wavenumbers(jm);
+      for (int n = m; n <= truncation; ++n) {
+        // Real component
+        for (atlas::idx_t jlev = 0; jlev < levels; jlev++) {
+          view(jnode, jlev) *= filterInverse[n];
+        }
+        jnode++;
+        // Imaginary component
+        for (atlas::idx_t jlev = 0; jlev < levels; jlev++) {
+          view(jnode, jlev) *= filterInverse[n];
+        }
+        jnode++;
+      }
+    }
+  }
+
+  oops::Log::trace() << classname() << "::leftInverseMultiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void SpectralAnalyticalFilter::print(std::ostream & os) const {
+  os << classname();
+}
+
+}  // namespace spectralb
+}  // namespace saber
---
+++ src/saber/spectralb/SpectralAnalyticalFilter.h
@@ -0,0 +1,98 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "eckit/config/LocalConfiguration.h"
+
+#include "oops/base/GeometryData.h"
+#include "oops/base/Variables.h"
+
+#include "saber/oops/SaberBlockParametersBase.h"
+#include "saber/oops/SaberOuterBlockBase.h"
+
+namespace saber {
+namespace spectralb {
+
+// -----------------------------------------------------------------------------
+
+class SpectralAnalyticalFilterParameters : public SaberBlockParametersBase {
+  OOPS_CONCRETE_PARAMETERS(SpectralAnalyticalFilterParameters, SaberBlockParametersBase)
+
+ public:
+    /// Whether to normalize as a localization function
+    oops::Parameter<bool> normalize{"normalize filter variance", true, this};
+    /// Filter specifications
+    oops::Parameter<eckit::LocalConfiguration> function{"function",
+                                                        eckit::LocalConfiguration(), this};
+
+    oops::Variables mandatoryActiveVars() const override {return oops::Variables();}
+};
+
+// -----------------------------------------------------------------------------
+
+class SpectralAnalyticalFilter : public SaberOuterBlockBase {
+ public:
+  static const std::string classname() {return "saber::spectralb::SpectralAnalyticalFilter";}
+
+  typedef SpectralAnalyticalFilterParameters Parameters_;
+
+  SpectralAnalyticalFilter(const oops::GeometryData &,
+                           const oops::Variables &,
+                           const eckit::Configuration &,
+                           const Parameters_ &,
+                           const atlas::FieldSet &,
+                           const atlas::FieldSet &,
+                           const util::DateTime &);
+
+  virtual ~SpectralAnalyticalFilter() = default;
+
+  const oops::GeometryData & innerGeometryData() const override {return innerGeometryData_;}
+  const oops::Variables & innerVars() const override {return innerVars_;}
+
+  void multiply(atlas::FieldSet &) const override;
+  void multiplyAD(atlas::FieldSet &) const override;
+  void leftInverseMultiply(atlas::FieldSet &) const override;
+
+  atlas::FieldSet generateInnerFieldSet(const oops::GeometryData & innerGeometryData,
+                                        const oops::Variables & innerVars,
+                                        const size_t & timeRank) const override
+    {return util::createSmoothFieldSet(innerGeometryData.comm(),
+                                       innerGeometryData.functionSpace(),
+                                       innerVars);}
+
+  atlas::FieldSet generateOuterFieldSet(const oops::GeometryData & outerGeometryData,
+                                        const oops::Variables & outerVars,
+                                        const size_t & timeRank) const override
+    {return util::createSmoothFieldSet(outerGeometryData.comm(),
+                                       outerGeometryData.functionSpace(),
+                                       outerVars);}
+
+
+ private:
+  void print(std::ostream &) const override;
+
+  /// Parameters
+  Parameters_ params_;
+  /// Active variables
+  const oops::Variables activeVars_;
+  /// inner Geometry Data for next block
+  const oops::GeometryData & innerGeometryData_;
+  /// inner variables for next block
+  const oops::Variables & innerVars_;
+  /// Spectral FunctionSpace
+  const atlas::functionspace::Spectral specFunctionSpace_;
+  /// Filter in spectral space
+  const std::vector<double> spectralFilter_;
+};
+
+}  // namespace spectralb
+}  // namespace saber
