87d5bb19397a98390e4c00aabcfb8f175386f526
Francois Hebert <hebert@ucar.edu>
2023-07-27 14:20:57 -0600

    Compile with C++17 (#639)
    Co-authored-by: Anna Shlyaeva <annash@ucar.edu>

--- cmake/saber_compiler_flags.cmake
+++ cmake/saber_compiler_flags.cmake
@@ -3,6 +3,16 @@
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS OFF)
+set(CMAKE_FORTRAN_STANDARD 08)
+set(CMAKE_FORTRAN_STANDARD_REQUIRED ON)
+set(CMAKE_FORTRAN_EXTENSIONS OFF)
+
 if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
   add_definitions( -DNDEBUG )
 endif( )
