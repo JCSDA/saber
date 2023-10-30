f9b3c1e31fa1db7030b646eebf88a85e0ec0d6ce
Chris Thomas <32307951+ctgh@users.noreply.github.com>
2023-10-30 16:58:34 +0000

    Block that writes fields to netCDF file (#697)
    * write out fields
    
    * coding norms
    
    * more comprehensive test
    
    * test reference
    
    * coding norms
    
    * change class name to WriteFields
    
    * enable specification of output path
    
    * update paths in test reference file
    
    * change where mo_hydrostatic output occurs

---
+++ src/saber/generic/WriteFields.cc
@@ -0,0 +1,154 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "saber/generic/WriteFields.h"
+
+#include <sys/stat.h>
+
+#include <sstream>
+#include <vector>
+
+#include "oops/base/FieldSet3D.h"
+#include "oops/util/FieldSetHelpers.h"
+#include "oops/util/FieldSetOperations.h"
+#include "oops/util/Logger.h"
+
+namespace saber {
+namespace generic {
+
+// -----------------------------------------------------------------------------
+
+static SaberOuterBlockMaker<WriteFields> makerWriteFields_("write fields");
+
+// -----------------------------------------------------------------------------
+
+void WriteFields::writeToFile(const atlas::FieldSet & fset,
+                              const std::string & description,
+                              size_t & count) const {
+  const auto & fieldNames = params_.fieldNames.value();
+
+  // Select the fields to write out.
+  // If the `fieldNames` list is empty, write out all fields in `fset`.
+  atlas::FieldSet fsetwrite;
+  for (const auto & field : fset) {
+    if (fieldNames.empty() ||
+        std::find(fieldNames.begin(), fieldNames.end(), field.name()) != fieldNames.end()) {
+      fsetwrite.add(field);
+    }
+  }
+
+  // Check there is at least one field to write out.
+  if (fsetwrite.size() == 0) {
+    oops::Log::warning() << "No fields present. File will not be written." << std::endl;
+    return;
+  }
+
+  // Produce a filename and increment the relevant counter.
+  std::stringstream filepath;
+  filepath << params_.outputPath.value() << "/"
+           << "fields_" << params_.label.value() << "_" << description << "_" << count++;
+  const std::string filepathnc = filepath.str() + ".nc";
+
+  eckit::LocalConfiguration conf;
+  conf.set("filepath", filepath.str());
+
+  // todo: replace the use of `stat` with `std::filesystem::exists`
+  // when compilers have been upgraded.
+  struct stat buffer;
+  if (stat(filepathnc.c_str(), &buffer) == 0) {
+    oops::Log::warning() << "File " << filepathnc << " already exists. Overwriting." << std::endl;
+  }
+
+  // Write field set to file.
+  util::writeFieldSet(innerGeometryData_.comm(), conf, fsetwrite);
+
+  // Output for validation.
+  oops::Log::test() << "Wrote file " << filepathnc << " with fields";
+  for (const auto & field : fsetwrite) {
+    oops::Log::test() << " " << field.name() << " ("
+                      << util::normField(field, innerGeometryData_.comm()) << ")";
+  }
+  oops::Log::test() << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+WriteFields::WriteFields(const oops::GeometryData & outerGeometryData,
+                         const oops::Variables & outerVars,
+                         const eckit::Configuration & covarConfig,
+                         const Parameters_ & params,
+                         const oops::FieldSet3D & xb,
+                         const oops::FieldSet3D & fg)
+  : SaberOuterBlockBase(params),
+    innerGeometryData_(outerGeometryData),
+    innerVars_(outerVars),
+    params_(params),
+    count_xb_(0),
+    count_fg_(0),
+    count_multiply_(0),
+    count_multiplyad_(0),
+    count_leftinversemultiply_(0)
+{
+  oops::Log::trace() << classname() << "::WriteFields starting" << std::endl;
+
+  if (params_.writeXb) {
+    writeToFile(xb.fieldSet(), "xb", count_xb_);
+  }
+
+  if (params_.writeFg) {
+    writeToFile(fg.fieldSet(), "fg", count_fg_);
+  }
+
+  oops::Log::trace() << classname() << "::WriteFields done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void WriteFields::multiply(atlas::FieldSet & fset) const {
+  oops::Log::trace() << classname() << "::multiply starting" << std::endl;
+
+  if (params_.writeMultiply) {
+    writeToFile(fset, "multiply", count_multiply_);
+  }
+
+  oops::Log::trace() << classname() << "::multiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void WriteFields::multiplyAD(atlas::FieldSet & fset) const {
+  oops::Log::trace() << classname() << "::multiplyAD starting" << std::endl;
+
+  if (params_.writeMultiplyAD) {
+    writeToFile(fset, "multiplyad", count_multiplyad_);
+  }
+
+  oops::Log::trace() << classname() << "::multiplyAD done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void WriteFields::leftInverseMultiply(atlas::FieldSet & fset) const {
+  oops::Log::trace() << classname() << "::leftInverseMultiply starting" << std::endl;
+
+  if (params_.writeLeftInverseMultiply) {
+    writeToFile(fset, "leftinversemultiply", count_leftinversemultiply_);
+  }
+
+  oops::Log::trace() << classname() << "::leftInverseMultiply done" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void WriteFields::print(std::ostream & os) const {
+  os << classname();
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace generic
+}  // namespace saber
---
+++ src/saber/generic/WriteFields.h
@@ -0,0 +1,102 @@
+/*
+ * (C) Crown Copyright 2023 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "atlas/field.h"
+
+#include "oops/base/GeometryData.h"
+
+#include "saber/blocks/SaberBlockParametersBase.h"
+#include "saber/blocks/SaberOuterBlockBase.h"
+
+namespace saber {
+namespace generic {
+
+// -----------------------------------------------------------------------------
+
+class WriteFieldsParameters : public SaberBlockParametersBase {
+  OOPS_CONCRETE_PARAMETERS(WriteFieldsParameters, SaberBlockParametersBase)
+
+ public:
+  oops::Variables mandatoryActiveVars() const override {return oops::Variables();}
+
+  /// Path of output file.
+  oops::RequiredParameter<std::string> outputPath{"output path", this};
+
+  /// Label to assign to output file.
+  oops::RequiredParameter<std::string> label{"label", this};
+
+  /// List of fields to write out.
+  /// If this parameter is empty (the default) then all fields contained in the
+  /// FieldSet are written out.
+  oops::Parameter<std::vector<std::string>> fieldNames{"field names", {}, this};
+
+  /// Write out fields, contained in xb, in the block's constructor.
+  oops::Parameter<bool> writeXb{"write xb", false, this};
+
+  /// Write out fields, contained in fg, in the block's constructor.
+  oops::Parameter<bool> writeFg{"write fg", false, this};
+
+  /// Write out fields in the block's multiply() routine.
+  oops::Parameter<bool> writeMultiply{"write multiply fset", false, this};
+
+  /// Write out fields in the block's multiplyAD() routine.
+  oops::Parameter<bool> writeMultiplyAD{"write multiplyad fset", false, this};
+
+  /// Write out fields in the block's leftInverseMultiply() routine.
+  oops::Parameter<bool> writeLeftInverseMultiply{"write leftinversemultiply fset", false, this};
+};
+
+// -----------------------------------------------------------------------------
+
+class WriteFields : public SaberOuterBlockBase {
+ public:
+  static const std::string classname() {return "saber::generic::WriteFields";}
+
+  typedef WriteFieldsParameters Parameters_;
+
+  WriteFields(const oops::GeometryData &,
+              const oops::Variables &,
+              const eckit::Configuration &,
+              const Parameters_ &,
+              const oops::FieldSet3D &,
+              const oops::FieldSet3D &);
+
+  virtual ~WriteFields() = default;
+
+  const oops::GeometryData & innerGeometryData() const override {return innerGeometryData_;}
+  const oops::Variables & innerVars() const override {return innerVars_;}
+
+  void multiply(atlas::FieldSet &) const override;
+  void multiplyAD(atlas::FieldSet &) const override;
+  void leftInverseMultiply(atlas::FieldSet &) const override;
+
+ private:
+  void print(std::ostream &) const override;
+
+  /// Write selected fields to a file.
+  void writeToFile(const atlas::FieldSet &, const std::string &, size_t &) const;
+
+  const oops::GeometryData & innerGeometryData_;
+  oops::Variables innerVars_;
+  const Parameters_ params_;
+  mutable size_t count_xb_;
+  mutable size_t count_fg_;
+  mutable size_t count_multiply_;
+  mutable size_t count_multiplyad_;
+  mutable size_t count_leftinversemultiply_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace generic
+}  // namespace saber
