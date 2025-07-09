85c7c033106760c346d89f5216040403a8f05c96
Francois Hebert <hebert@ucar.edu>
2025-07-09 08:39:20 -0700

    Use for_each in moisture control saber block loops (#1069)
    * Use for_each in moisture control saber block loops
    
    * Code review: clean up lambda capture clause

--- src/saber/vader/MoistureControl.cc
+++ src/saber/vader/MoistureControl.cc
@@ -20,6 +20,7 @@
 
 #include "oops/base/FieldSet3D.h"
 #include "oops/base/Variables.h"
+#include "oops/util/for_each.h"
 #include "oops/util/Timer.h"
 
 #include "saber/blocks/SaberOuterBlockBase.h"
@@ -32,119 +33,104 @@ namespace vader {
 
 namespace {
 
-using atlas::array::make_view;
-using atlas::idx_t;
-
 void eval_moisture_control_inv_tl(atlas::FieldSet & incFlds,
                                   const atlas::FieldSet & augStateFlds) {
   // Using Cramer's rule to calculate inverse.
-  auto muRecipDeterView = make_view<const double, 2>(augStateFlds["muRecipDeterminant"]);
-  auto muRow1Column1View = make_view<const double, 2>(augStateFlds["muRow1Column1"]);
-  auto muRow1Column2View = make_view<const double, 2>(augStateFlds["muRow1Column2"]);
-  auto muRow2Column1View = make_view<const double, 2>(augStateFlds["muRow2Column1"]);
-  auto muRow2Column2View  = make_view<const double, 2>(augStateFlds["muRow2Column2"]);
-  auto muIncView = make_view<const double, 2>(incFlds["mu"]);
-  auto thetavIncView = make_view<const double, 2>(incFlds["virtual_potential_temperature"]);
-  auto qtIncView = make_view<double, 2>(incFlds["qt"]);
-  auto thetaIncView = make_view<double, 2>(incFlds["air_potential_temperature"]);
+  util::for_each_value(
+      [](const double muRecipDet,
+         const double muRow1Column1,
+         const double muRow1Column2,
+         const double muRow2Column1,
+         const double muRow2Column2,
+         const double muInc,
+         const double thetavInc,
+         double & qtInc,
+         double & thetaInc) {
+        // VAR equivalent in Var_UpPFtheta_qT.f90 for thetaIncView
+        // (beta2 * muA * theta_v' +   beta1 * mu') /
+        // (alpha1 * beta2 * muA - alpha2 * muA * beta1)
+        thetaInc = muRecipDet * (muRow1Column1 * thetavInc - muRow2Column1 * muInc);
+        // VAR equivalent in Var_UpPFtheta_qT.f90 for qtIncView
+        // (alpha1 * mu_v' -   alpha2 * muA * thetav') /
+        // (alpha1 * beta2 * muA - alpha2 * muA * beta1)
+        qtInc = muRecipDet * (muRow2Column2 * muInc - muRow1Column2 * thetavInc);
+      },
+      augStateFlds["muRecipDeterminant"],
+      augStateFlds["muRow1Column1"],
+      augStateFlds["muRow1Column2"],
+      augStateFlds["muRow2Column1"],
+      augStateFlds["muRow2Column2"],
+      incFlds["mu"],
+      incFlds["virtual_potential_temperature"],
+      incFlds["qt"],
+      incFlds["air_potential_temperature"]);
 
-  const atlas::idx_t n_levels(incFlds["mu"].shape(1));
-  const idx_t sizeOwned =
-        util::getSizeOwned(incFlds["mu"].functionspace());
-  atlas_omp_parallel_for(idx_t ih = 0; ih < sizeOwned; ++ih) {
-    for (idx_t ilev = 0; ilev < n_levels; ++ilev) {
-      // VAR equivalent in Var_UpPFtheta_qT.f90 for thetaIncView
-      // (beta2 * muA * theta_v' +   beta1 * mu') /
-      // (alpha1 * beta2 * muA - alpha2 * muA * beta1)
-      thetaIncView(ih, ilev) = muRecipDeterView(ih, ilev) * (
-                             muRow1Column1View(ih, ilev) * thetavIncView(ih, ilev)
-                           - muRow2Column1View(ih, ilev) * muIncView(ih, ilev) );
-
-      // VAR equivalent in Var_UpPFtheta_qT.f90 for qtIncView
-      // (alpha1 * mu_v' -   alpha2 * muA * thetav') /
-      // (alpha1 * beta2 * muA - alpha2 * muA * beta1)
-      qtIncView(ih, ilev) = muRecipDeterView(ih, ilev) * (
-                           muRow2Column2View(ih, ilev) * muIncView(ih, ilev) -
-                           muRow1Column2View(ih, ilev) * thetavIncView(ih, ilev) );
-    }
-  }
-  incFlds["air_potential_temperature"].set_dirty();
   incFlds["qt"].set_dirty();
+  incFlds["air_potential_temperature"].set_dirty();
 }
 
 void eval_moisture_control_inv_ad(atlas::FieldSet & hatFlds,
                                   const atlas::FieldSet & augStateFlds) {
-  auto muRecipDeterView = make_view<const double, 2>(augStateFlds["muRecipDeterminant"]);
-  auto muRow1Column1View = make_view<const double, 2>(augStateFlds["muRow1Column1"]);
-  auto muRow1Column2View = make_view<const double, 2>(augStateFlds["muRow1Column2"]);
-  auto muRow2Column1View = make_view<const double, 2>(augStateFlds["muRow2Column1"]);
-  auto muRow2Column2View  = make_view<const double, 2>(augStateFlds["muRow2Column2"]);
-  auto qtHatView = make_view<double, 2>(hatFlds["qt"]);
-  auto muHatView = make_view<double, 2>(hatFlds["mu"]);
-  auto thetavHatView = make_view<double, 2>(hatFlds["virtual_potential_temperature"]);
-  auto thetaHatView = make_view<double, 2>(hatFlds["air_potential_temperature"]);
+  util::for_each_value(
+      [](const double muRecipDet,
+         const double muRow1Column1,
+         const double muRow1Column2,
+         const double muRow2Column1,
+         const double muRow2Column2,
+         double & qtHat,
+         double & muHat,
+         double & thetavHat,
+         double & thetaHat) {
+        thetavHat += muRecipDet * (muRow1Column1 * thetaHat - muRow1Column2 * qtHat);
+        muHat += muRecipDet * (muRow2Column2 * qtHat - muRow2Column1 * thetaHat);
+        thetaHat = 0.0;
+        qtHat = 0.0;
+      },
+      augStateFlds["muRecipDeterminant"],
+      augStateFlds["muRow1Column1"],
+      augStateFlds["muRow1Column2"],
+      augStateFlds["muRow2Column1"],
+      augStateFlds["muRow2Column2"],
+      hatFlds["qt"],
+      hatFlds["mu"],
+      hatFlds["virtual_potential_temperature"],
+      hatFlds["air_potential_temperature"]);
 
-  const atlas::idx_t n_levels(hatFlds["mu"].shape(1));
-  const idx_t sizeOwned =
-        util::getSizeOwned(hatFlds["mu"].functionspace());
-  atlas_omp_parallel_for(idx_t ih = 0; ih < sizeOwned; ++ih) {
-    for (idx_t ilev = 0; ilev < n_levels; ++ilev) {
-      thetavHatView(ih, ilev) += muRecipDeterView(ih, ilev) *
-                                 muRow1Column1View(ih, ilev) * thetaHatView(ih, ilev);
-      muHatView(ih, ilev) -= muRecipDeterView(ih, ilev) *
-                             muRow2Column1View(ih, ilev) * thetaHatView(ih, ilev);
-      thetavHatView(ih, ilev) -= muRecipDeterView(ih, ilev) *
-                                 muRow1Column2View(ih, ilev) * qtHatView(ih, ilev);
-      muHatView(ih, ilev) += muRecipDeterView(ih, ilev) *
-                             muRow2Column2View(ih, ilev) * qtHatView(ih, ilev);
-      thetaHatView(ih, ilev) = 0.0;
-      qtHatView(ih, ilev) = 0.0;
-    }
-  }
-  hatFlds["air_potential_temperature"].set_dirty();
   hatFlds["qt"].set_dirty();
   hatFlds["mu"].set_dirty();
   hatFlds["virtual_potential_temperature"].set_dirty();
+  hatFlds["air_potential_temperature"].set_dirty();
 }
 
 
 void eval_moisture_control_tl(atlas::FieldSet & incFlds,
                               const atlas::FieldSet & augStateFlds) {
-  auto muRow1Column1View = make_view<const double, 2>(augStateFlds["muRow1Column1"]);
-  auto muRow1Column2View = make_view<const double, 2>(augStateFlds["muRow1Column2"]);
-  auto muRow2Column1View = make_view<const double, 2>(augStateFlds["muRow2Column1"]);
-  auto muRow2Column2View = make_view<const double, 2>(augStateFlds["muRow2Column2"]);
-  auto thetaIncView = make_view<const double, 2>(incFlds["air_potential_temperature"]);
-  auto qtIncView = make_view<const double, 2>(incFlds["qt"]);
-  auto muIncView = make_view<double, 2>(incFlds["mu"]);
-  auto thetavIncView = make_view<double, 2>(incFlds["virtual_potential_temperature"]);
+  util::for_each_value(
+      [](const double muRow1Column1,
+         const double muRow1Column2,
+         const double muRow2Column1,
+         const double muRow2Column2,
+         const double thetaInc,
+         const double qtInc,
+         double & muInc,
+         double & thetavInc) {
+        muInc = muRow1Column1 * qtInc + muRow1Column2 * thetaInc;
+        thetavInc = muRow2Column1 * qtInc + muRow2Column2 * thetaInc;
+      },
+      augStateFlds["muRow1Column1"],
+      augStateFlds["muRow1Column2"],
+      augStateFlds["muRow2Column1"],
+      augStateFlds["muRow2Column2"],
+      incFlds["air_potential_temperature"],
+      incFlds["qt"],
+      incFlds["mu"],
+      incFlds["virtual_potential_temperature"]);
 
-  const idx_t n_levels(incFlds["mu"].shape(1));
-  const idx_t sizeOwned =
-        util::getSizeOwned(incFlds["mu"].functionspace());
-  atlas_omp_parallel_for(idx_t ih = 0; ih < sizeOwned; ++ih) {
-    for (idx_t ilev = 0; ilev < n_levels; ++ilev) {
-      muIncView(ih, ilev) = muRow1Column1View(ih, ilev) * qtIncView(ih, ilev)
-                          + muRow1Column2View(ih, ilev) * thetaIncView(ih, ilev);
-      thetavIncView(ih, ilev) = muRow2Column1View(ih, ilev) * qtIncView(ih, ilev)
-                            + muRow2Column2View(ih, ilev) * thetaIncView(ih, ilev);
-    }
-  }
   incFlds["mu"].set_dirty();
   incFlds["virtual_potential_temperature"].set_dirty();
 }
 
 void eval_moisture_control_traj(atlas::FieldSet & fields) {
-  auto qtView = make_view<const double, 2>(fields["qt"]);
-  auto qView = make_view<const double, 2>(
-                              fields["water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water"]);
-  auto thetaView = make_view<const double, 2>(fields["air_potential_temperature"]);
-  auto exnerView = make_view<const double, 2>(fields["dimensionless_exner_function"]);
-  auto dlsvpdTView = make_view<const double, 2>(fields["dlsvpdT"]);
-  auto qsatView = make_view<const double, 2>(fields["qsat"]);
-  auto muAView = make_view<const double, 2>(fields["muA"]);
-  auto muH1View = make_view<const double, 2>(fields["muH1"]);
-
   // muRow1Column1, muRow1Column2, muRow2Column1, muRow2Column2
   // are coefficients of a (2x2) matrix = A
   //  (mu')       = A (qt')     where A is
@@ -154,30 +140,41 @@ void eval_moisture_control_traj(atlas::FieldSet & fields) {
   //  (                                                    )
   //  (c_v theta     (1 + cv q)                            )
   //
-  auto muRow1Column1View = make_view<double, 2>(fields["muRow1Column1"]);
-  auto muRow1Column2View = make_view<double, 2>(fields["muRow1Column2"]);
-  auto muRow2Column1View = make_view<double, 2>(fields["muRow2Column1"]);
-  auto muRow2Column2View = make_view<double, 2>(fields["muRow2Column2"]);
-  auto muRecipDeterminantView = make_view<double, 2>(fields["muRecipDeterminant"]);
+  util::for_each_value(
+      [=](const double qt,
+          const double q,
+          const double theta,
+          const double exner,
+          const double dlsvpdT,
+          const double qsat,
+          const double muA,
+          const double muH1,
+          double & muRow1Column1,
+          double & muRow1Column2,
+          double & muRow2Column1,
+          double & muRow2Column2,
+          double & muRecipDet) {
+        // the comments below are there to allow checking with the VAR code.
+        muRow1Column1 = muA / qsat;  // beta2 * muA
+        muRow1Column2 = - qt * muH1 * exner * dlsvpdT * muRow1Column1;  // alpha2 * muA
+        muRow2Column1 = ::mo::constants::c_virtual * theta;  // beta1
+        muRow2Column2 = 1.0 + ::mo::constants::c_virtual * q;  // alpha1
+        muRecipDet = 1.0 / (muRow2Column2 * muRow1Column1 - muRow1Column2 * muRow2Column1);
+      },
+      fields["qt"],
+      fields["water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water"],
+      fields["air_potential_temperature"],
+      fields["dimensionless_exner_function"],
+      fields["dlsvpdT"],
+      fields["qsat"],
+      fields["muA"],
+      fields["muH1"],
+      fields["muRow1Column1"],
+      fields["muRow1Column2"],
+      fields["muRow2Column1"],
+      fields["muRow2Column2"],
+      fields["muRecipDeterminant"]);
 
-  // the comments below are there to allow checking with the VAR code.
-  const idx_t n_levels(fields["air_potential_temperature"].shape(1));
-  const idx_t sizeOwned =
-        util::getSizeOwned(fields["air_potential_temperature"].functionspace());
-  atlas_omp_parallel_for(idx_t ih = 0; ih < sizeOwned; ++ih) {
-    for (idx_t ilev = 0; ilev < n_levels; ++ilev) {
-      muRow1Column1View(ih, ilev) = muAView(ih, ilev) / qsatView(ih, ilev);  // beta2 * muA
-      muRow1Column2View(ih, ilev) = - qtView(ih, ilev)  * muH1View(ih, ilev)
-        * exnerView(ih, ilev) * dlsvpdTView(ih, ilev) * muRow1Column1View(ih, ilev);
-      // alpha2 * muA
-      muRow2Column1View(ih, ilev) = ::mo::constants::c_virtual * thetaView(ih, ilev);   // beta1
-      muRow2Column2View(ih, ilev) = 1.0 + ::mo::constants::c_virtual * qView(ih, ilev);  // alpha1
-      muRecipDeterminantView(ih, ilev) = 1.0 /(
-        muRow2Column2View(ih, ilev) * muRow1Column1View(ih, ilev)
-        - muRow1Column2View(ih, ilev) * muRow2Column1View(ih, ilev));
-           // 1/( alpha1 * beta2 * muA - alpha2 * muA * beta1)
-    }
-  }
   fields["muRow1Column1"].set_dirty();
   fields["muRow1Column2"].set_dirty();
   fields["muRow2Column1"].set_dirty();
