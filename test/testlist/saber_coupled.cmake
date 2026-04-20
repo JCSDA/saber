8fc9811013a8fc8e3efcfefd3696986782e6f2f6
Nate Crossette <ncrossette@ucar.edu>
2026-04-20 15:36:31 -0600

    Add repo name to new tests (#1229)
    * Add repo name to new tests
    
    * fix dependencies
    
    * change MPI_OMP configuration

--- test/testlist/saber_coupled.cmake
+++ test/testlist/saber_coupled.cmake
@@ -1,16 +1,16 @@
 # coupled_dirac_id
-saber_add_test( TARGET coupled_dirac_id_1-1
+saber_add_test( TARGET saber_coupled_dirac_id_1-1
                 MPI 1
                 OMP 1
                 COMMAND ${CMAKE_BINARY_DIR}/bin/saber_quench_coupled_error_covariance_toolbox.x
                 ARGS testinput/coupled_dirac_id.yaml
                 DEPENDS saber_quench_coupled_error_covariance_toolbox.x
-                TEST_DEPENDS error_covariance_training_diffusion_2 )
+                TEST_DEPENDS error_covariance_training_diffusion_2_1-1 )
 
-saber_add_test( TARGET coupled_dirac_id_1-2
-                MPI 1
-                OMP 2
+saber_add_test( TARGET saber_coupled_dirac_id_2-1
+                MPI 2
+                OMP 1
                 COMMAND ${CMAKE_BINARY_DIR}/bin/saber_quench_coupled_error_covariance_toolbox.x
                 ARGS testinput/coupled_dirac_id.yaml
                 DEPENDS saber_quench_coupled_error_covariance_toolbox.x
-                TEST_DEPENDS error_covariance_training_diffusion_2 )
+                TEST_DEPENDS error_covariance_training_diffusion_2_2-1 )
